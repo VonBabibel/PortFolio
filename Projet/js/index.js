115a811a2ef78cc108c55a5e1891fd3f3d39fdc8
VonBabibel <vincent.hebroux@gmail.com>
2016-11-17 15:20:18 +0100

    Add files via upload

---
+++ Projet/js/index.js
@@ -0,0 +1,20 @@
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!document.querySelector('.Contact1').contains(event.target)) {
+
+    var dropdowns = document.getElementsByClassName("Contact");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
